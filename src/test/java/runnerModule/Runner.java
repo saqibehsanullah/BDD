023989b1d3df631f3ce93cc9dabe5c949c94fe87
saqibehsanullah <saqibehsanullah@gmail.com>
2023-05-15 12:40:32 +0530

    new project

--- src/test/java/runnerModule/Runner.java
+++ src/test/java/runnerModule/Runner.java
@@ -1,7 +1,7 @@
 package runnerModule;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(tags = "@Smoke", features = {"src/test/java/features/"}, glue = {"stepDefinitions"},
+@CucumberOptions(tags = "@Regression", features = {"src/test/java/features/"}, glue = {"stepDefinitions"},
         plugin = {})
 public class Runner extends AbstractTestNGCucumberTests {
 
